bffb01a5b41ee8d1989ead838686138ad23ddb8c
Pritam <mondalpritam819@gmail.com>
2022-01-12 11:25:23 +0530

    Preventing Sql Injection and XSS attack

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ var mysql = require('mysql');
 var app = express();
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var validator = require('validator');
+var session = require('express-session');
 
 var masterModel = require("./masterModel");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+
+// xss attack prevention by preventing cookie access by browser js scripts
+app.use(session({
+    secret: "k5Zurj4",
+    cookie: {
+        httpOnly: true,
+        secure: true
+    },
+    resave: true,
+    saveUninitialized: true
+})
+)
 
 var options = {
     'client1': {
@@ -50,8 +64,9 @@ connection.connect(function (error) {
 
 app.post('/verifyemployee/', function (req, resp) {
 
-    employeeid = (req.body.employeeid)
-    domainid = String(req.body.employeeid).split('@')[1];
+    // xss attack prevention of string coming from user input using validator
+    employeeid = validator.escape(req.body.employeeid);
+    domainid = String(employeeid).split('@')[1];
 
     console.log(employeeid)
 
@@ -86,24 +101,24 @@ app.post('/get_current_details/', function (req, resp) {
         console.log(result_session);
 
         for (const key in result_session) {
-        if (err) {
-            console.log(err);
-          }
-          else {
-            var responseData = {
-              name: result_session[key].name,
-              email: result_session[key].email,
-              phone: result_session[key].phone,
-              organization_name: result_session[key].organization_name
+            if (err) {
+                console.log(err);
             }
-      
-            // console.log(responseData);
-            
-          }
+            else {
+                var responseData = {
+                    name: result_session[key].name,
+                    email: result_session[key].email,
+                    phone: result_session[key].phone,
+                    organization_name: result_session[key].organization_name
+                }
+
+                // console.log(responseData);
+
+            }
         }
         resp.send(JSON.stringify(responseData));
     });
 
 });
 
-app.listen(4000, () => console.log('Server started on port 4000'));
+app.listen(4000);
